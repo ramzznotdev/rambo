45f929b7789c4a6d8c976015ce88dcc52d697414
ramzznotdev <ramzznotdev@gmail.com>
2026-04-06 22:45:19 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,4 +1,4 @@
 global.feenya = 500 //atur fee nya misal harga asli 1.480 + fee 200 jadi harganya 1.680
-global.apikey = "KhafaTopUp_tabv4nukyceqsxb2" //isi apikey dari https://ciaatopup.my.id minta verifikasi h2h ke admin nya
+global.apikey = "KhafaTopUp_e5w37g6lyv5nehys" //isi apikey dari https://ciaatopup.my.id minta verifikasi h2h ke admin nya
 
 
